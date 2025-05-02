1714b8ffe12fdd00e159d5830a8f44db75dbde05
Neveous <badyrova.kristin@mail.ru>
2025-05-02 23:22:01 +0300

    2 upd

--- src/test/java/ArithmeticOperationsTest.java
+++ src/test/java/ArithmeticOperationsTest.java
@@ -6,18 +6,22 @@ public class ArithmeticOperationsTest {
     public void add_validInput() {
         assertEquals(ArithmeticOperations.add(2, 3), 5);
     }
+
     @Test
     public void subtract_validInput() {
         assertEquals(ArithmeticOperations.subtract(2, 3), -1);
     }
+
     @Test
     public void multiply_validInput() {
         assertEquals(ArithmeticOperations.multiply(2, 3), 6);
     }
+
     @Test
     public void divide_validInput() {
         assertEquals(ArithmeticOperations.divide(6, 3), 2.0);
     }
+
     @Test(expectedExceptions = IllegalArgumentException.class)
     public void divide_byZero() {
         ArithmeticOperations.divide(6, 0);
